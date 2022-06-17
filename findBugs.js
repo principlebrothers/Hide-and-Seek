bfe8ae31b777222196a3457a0bee817a0e5d2422
Ernest <92553079+principlebrothers@users.noreply.github.com>
2022-06-17 16:45:44 +0000

    Update findBugs.js

--- findBugs.js
+++ findBugs.js
@@ -1,1 +1,23 @@
+let arr = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]
+function migratoryBirds(arr) {
+    // Write your code here
+    let grouped = arr.reduce((prev, curr) =>{
+      (prev[curr]) ?prev[curr]+= 1:prev[curr] = 1;
+      return prev;
+    }{})
+    
+     let max = 0;
+    let maxKey = "";
 
+for(let group in grouped){
+  console.log(group)
+  if(grouped[group] > max){
+    console.log(grouped(group))
+    max = grouped[group];
+    maxKey= group
+  }
+}
+ parseInt(maxKey)
+}
+
+console.log(migratoryBirds(.arr));
